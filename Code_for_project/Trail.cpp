9af8e0d89fc92935aff4c29f09ddac59d59f74c4
MuneerAlrawahi <69582806+MuneerAlrawahi@users.noreply.github.com>
2023-11-09 23:45:21 -0700

    Some updates

--- Code_for_project/Trail.cpp
+++ Code_for_project/Trail.cpp
@@ -22,37 +22,36 @@ using namespace deme;
 using namespace std::filesystem;
 
 int main() {
+
+
+    // Initialize the DEMSolver
     DEMSolver DEMSim;
     DEMSim.SetVerbosity(STEP_METRIC);
-    // For general use cases, you want to set the verbosity to INFO: It's also a bit faster than STEP_METRIC.
-    // DEMSim.SetVerbosity(INFO);
     DEMSim.SetOutputFormat(OUTPUT_FORMAT::CSV);
     DEMSim.SetOutputContent(OUTPUT_CONTENT::ABSV);
     DEMSim.SetMeshOutputFormat(MESH_FORMAT::VTK);
 
-    // If you don't need individual force information, then this option makes the solver run a bit faster.
+    
 
+    //Define materials propeties 
     // E, nu, CoR, mu, Crr...
     auto mat_type_Plate = DEMSim.LoadMaterial({{"E", 1e8}, {"nu", 0.3}, {"CoR", 0.6}, {"mu", 0.5}, {"Crr", 0.0}});
     auto mat_type_granular = DEMSim.LoadMaterial({{"E", 1e8}, {"nu", 0.3}, {"CoR", 0.6}, {"mu", 0.2}, {"Crr", 0.0}});
-    // If you don't have this line, then mu between mixer material and granular material will be 0.35 (average of the
-    // two).
     DEMSim.SetMaterialPropertyPair("mu", mat_type_Plate, mat_type_granular, 0.5);
 
+
+
+    // Define the simulation domain and boundary conditions 
     float step_size = 1e-5;
     double world_size = 3;
     DEMSim.InstructBoxDomainDimension({0, world_size}, {0, world_size}, {0, world_size});
     DEMSim.InstructBoxDomainBoundingBC("top_open", mat_type_granular);
 
-    // Now add a box boundary
-    // auto template_granular = DEMSim.LoadSphereType(granular_rad * granular_rad * granular_rad * 2.8e3 * 4 / 3 * 3.14,
-    //                                                granular_rad, mat_type_granular);
-    // Calculate its mass and MOI
-
+    // Define the plate and set up its postion 
     auto projectile = DEMSim.AddWavefrontMeshObject((GET_DATA_PATH() / "mesh/plate_1by1.obj").string(), mat_type_Plate);
     std::cout << "Total num of triangles: " << projectile->GetNumTriangles() << std::endl;
 
-    projectile->SetInitPos(make_float3(world_size / 2, world_size / 2, -world_size /  2));
+    projectile->SetInitPos(make_float3(world_size / 2, world_size / 2, -0.5));
     float plate_mass = 7.8e3 ;
     projectile->SetMass(plate_mass);
     projectile->SetMOI(make_float3(plate_mass * 2 / 5, plate_mass * 2 / 5, plate_mass * 2 / 5));
@@ -60,14 +59,12 @@ int main() {
     DEMSim.SetFamilyFixed(2);
 
 
-
+// Define the granular materrials 
     float terrain_rad = 0.25;
     auto template_terrain = DEMSim.LoadSphereType(terrain_rad * terrain_rad * terrain_rad * 2.6e3 * 4 / 3 * 3.14,
-                                                  terrain_rad, mat_type_terrain);
-
+                                                  terrain_rad, mat_type_granular);
     // Track the projectile
     auto proj_tracker = DEMSim.Track(projectile);
-
     float sample_halfheight = world_size / 8;
     float3 sample_center = make_float3(world_size / 2, world_size / 2, sample_halfheight + 0.05);
     float sample_halfwidth = world_size / 2 * 0.95;
@@ -79,12 +76,16 @@ int main() {
 
 
 
+
+
+
+
     DEMSim.SetInitTimeStep(step_size);
     DEMSim.SetGravitationalAcceleration(make_float3(0, 0, -9.81));
     DEMSim.SetCDUpdateFreq(40);
 
 
-
+    // Start the sumiluation 
     DEMSim.Initialize();
 
     path out_dir = current_path();
